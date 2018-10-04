b4405f5fc3c665b006416a3d3550c174bacc9945
Paul <paulusman@live.nl>
2018-10-04 23:56:40 +0200

    Added MVC structure with HomeController and Index view

--- Sk8er_Webshop/Startup.cs
+++ Sk8er_Webshop/Startup.cs
@@ -15,11 +15,20 @@ namespace Sk8er_Webshop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
