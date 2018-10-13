f0161037d908afe7d06591ba263e51ec83089234
Paul <paulusman@live.nl>
2018-10-13 15:28:25 +0200

    Created viewmodels, hardcoded product list as test, placed in view

---
+++ Sk8er_Webshop/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sk8er_Webshop.Models;
+using Sk8er_Webshop.ViewModels;
+
+namespace Sk8er_Webshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("All");
+        }
+
+        public IActionResult All()
+        {
+            var Products = new List<Product>
+            {
+                new Product {Name = "Kanariegele trui"},
+                new Product {Name = "Lelijke broek"},
+                new Product {Name = "Vans 200x"}
+            };
+
+            var viewModel = new AllProductViewModel
+            {
+                products = Products
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Sk8er_Webshop/ViewModels/AllProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Sk8er_Webshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sk8er_Webshop.ViewModels
+{
+    public class AllProductViewModel
+    {
+        public List<Product> products { get; set; }
+    }
+}
