d97d9293a0dbc23ea68ae5c11e7ff77bfc10dd4a
Paul <paulusman@live.nl>
2018-10-13 14:58:29 +0200

    Properties voor Product model added

--- Sk8er_Webshop.Models/Product.cs
+++ Sk8er_Webshop.Models/Product.cs
@@ -4,6 +4,13 @@ namespace Sk8er_Webshop.Models
 {
     public class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public string Collection { get; set; }
+
+        public string ProductType { get; set; }
+
     }
 }
