5a2d2db4bcb2fb8ec254dea8a73b02a2529cd444
Paul <paulusman@live.nl>
2018-10-17 20:22:39 +0200

    Connects to database

---
+++ Sk8er_Webshop.Data/DatabaseConnector.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+
+namespace Sk8er_Webshop.Data
+{
+    public class DatabaseConnector
+    {
+        private string connectionString { get; }
+        
+
+        public DatabaseConnector(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public string GetSingleString(string query)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            //Returns the first string from a query
+            con.Close();
+            con.Open();
+            SqlCommand command = new SqlCommand(query, con);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string returnString = reader.GetString(0);
+                    con.Close();
+
+                    return returnString;
+
+                }
+            }
+
+            con.Close();
+            return "";
+        }
+    }
+}
--- Sk8er_Webshop.Data/ProductData.cs
+++ Sk8er_Webshop.Data/ProductData.cs
@@ -1,12 +1,19 @@
-﻿using Sk8er_Webshop.Models;
+﻿using System;
+using Sk8er_Webshop.Models;
 
 namespace Sk8er_Webshop.Data
 {
     public static class ProductData
     {
+        private static string s = @"Data Source=sk8erwebshopdbserver.database.windows.net;Initial Catalog=Sk8erWebshop_database;User ID=TheAnswer42;Password=XTqwj]Q^`""NPh6*~s4t#PRE@t'7w~jy[.9#S>XrsP[*+JT,F(e3>&uP?syDBxE/*e]WE'^c&TDPwW^r2J""H<?tzQV6v`'2h]CK%b?(44C4aX8&`+Yx?QCA5XGpRX:{t;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public static Product GetProductById(int id)
         {
-            return new Product{Name = "TEST", ImgURL = "https://cdn.webshopapp.com/shops/260322/files/222595769/patagonia-patagonia-retro-pile-vest-el-cap-khaki-2.jpg" };
+            string query = "SELECT Name FROM Products WHERE ProductKey = 1;";
+            DatabaseConnector dbConnector = new DatabaseConnector(s);
+            string test = dbConnector.GetSingleString(query);
+            Console.WriteLine(test);
+
+            return new Product{Name = test, Description = "test", ImgURL = "https://cdn.webshopapp.com/shops/260322/files/222595769/patagonia-patagonia-retro-pile-vest-el-cap-khaki-2.jpg" };
         }
     }
 }
