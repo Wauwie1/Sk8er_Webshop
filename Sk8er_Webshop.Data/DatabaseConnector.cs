a2b7420c5a200c9d076d33dd85497426a9cbfbc9
Paul <paulusman@live.nl>
2018-10-26 13:51:07 +0200

    Deleted unnecesarry Sizes class

--- Sk8er_Webshop.Data/DatabaseConnector.cs
+++ Sk8er_Webshop.Data/DatabaseConnector.cs
@@ -8,6 +8,7 @@ namespace Sk8er_Webshop.Data
         private static string connectionString = @"Data Source=sk8erwebshopdbserver.database.windows.net;Initial Catalog=Sk8erWebshop_database;User ID=TheAnswer42;Password=XTqwj]Q^`""NPh6*~s4t#PRE@t'7w~jy[.9#S>XrsP[*+JT,F(e3>&uP?syDBxE/*e]WE'^c&TDPwW^r2J""H<?tzQV6v`'2h]CK%b?(44C4aX8&`+Yx?QCA5XGpRX:{t;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         
 
+        //TODO: Remove this method
         public static string GetSingleString(string query)
         {
             SqlConnection con = new SqlConnection(connectionString);
